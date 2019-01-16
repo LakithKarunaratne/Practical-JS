dd4b375f78ce8d80f7a9a25aa4ebc17e7ac3edbc
Lakith <lakith.hos@outlook.com>
2019-01-16 15:19:56 +0530

    Create while-loops.js

---
+++ while-loops.js
@@ -0,0 +1,14 @@
+
+let count = 1;
+
+while (count < 5) {
+	console.log(count);
+	count ++ ;
+}
+
+let count1 = 0; 
+while (count1 <= 5) {
+	console.log(count1);
+	count1 ++ ;
+}
+// always check your variables for infinite loops
