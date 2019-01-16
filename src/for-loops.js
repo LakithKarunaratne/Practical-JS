44f89bf3d4d7bd7222ca9278df8140d676a98cf3
Lakith <lakith.hos@outlook.com>
2019-01-16 15:23:44 +0530

    Create for-loops.js

---
+++ src/for-loops.js
@@ -0,0 +1,13 @@
+
+// Looping with For()
+
+for (let i = 0; i < 3; i++){
+	console.log(i);
+};
+
+var x = 12 // can declare outside the loop
+for (x; x < 16; x++){
+	console.log(x);
+};
+
+// be deciplined with the semicolons
